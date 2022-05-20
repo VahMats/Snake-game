a3912f0b89803cf6c9b3369f95d0efdf2289ad92
VahMats <vah.mats01@gmail.com>
2022-05-20 15:00:48 +0400

    first try on host

--- back/index.js
+++ back/index.js
@@ -3,10 +3,17 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const path = require("path")
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
 const io = new Server(server, {
     cors: {
